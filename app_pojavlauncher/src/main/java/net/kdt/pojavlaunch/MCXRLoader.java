31dcbba3be8b3713569d97aa0757a2986325615c
Judge <colepatrickroberts@gmail.com>
2022-12-18 19:12:36 -0800

    Final YVR fixes

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MCXRLoader.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/MCXRLoader.java
@@ -3,11 +3,10 @@ package net.kdt.pojavlaunch;
 import android.content.Context;
 
 public class MCXRLoader {
-    public static native void setAndroidInitInfo(Context ctx);
-
     static {
         System.loadLibrary("openvr_api");
     }
 
     public static native void launch(MainActivity activity);
+    public static native void setAndroidInitInfo(Context ctx);
 }
